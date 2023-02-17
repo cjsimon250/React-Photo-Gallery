fca329f5b02c6c247a66c655b0a0b6547bb9c07e
CJ Simon <cjsimon250@outlook.com>
2023-02-17 16:47:26 -0600

    added get request in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
 import GalleryForm from "../GalleryForm/GalleryForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  let [galleryList, setGalleryList] = useState([]);
+
+  //on load, get gallery
+  useEffect(() => {
+    getGalleryList();
+  }, []);
+
+  const getGalleryList = () => {
+    axios
+      .get("/gallery")
+      .then((response) => {
+        setGalleryList(response.data);
+        console.log(galleryList);
+      })
+      .catch((error) => {
+        alert("error in get GalleryList");
+        console.log("error in get GalleryList:", error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
