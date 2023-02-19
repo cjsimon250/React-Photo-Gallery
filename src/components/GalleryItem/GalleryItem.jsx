d186a9e697aab2316c0b46cabd2482f274b99f9e
CJ Simon <cjsimon250@outlook.com>
2023-02-18 23:01:54 -0600

    added all functionality for conditional rendering on image click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 function GalleryItem({ item, getGalleryList }) {
-  return (
-    <>
-      <img src={item.path}></img>
-      <p>{item.description}</p>
-      <p>Likes: {item.likes}</p>
-    </>
-  );
+  let [beenClicked, setBeenClicked] = useState(false);
+
+  function showDescription() {
+    setBeenClicked(true);
+  }
+  function showImage() {
+    setBeenClicked(false);
+  }
+  if (beenClicked === false) {
+    return (
+      <>
+        <img src={item.path} onClick={showDescription}></img>
+        <p>{item.description}</p>
+        <p>Likes: {item.likes}</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>{item.description}</p>
+        <p>Likes: {item.likes}</p>
+        <button onClick={showImage}>Show Picture</button>
+      </>
+    );
+  }
 }
 
 export default GalleryItem;
